883c5d793c44d56b34e700cbf26bba9f347f63cb
mckaigedj <dylan.j.mckaige@gmail.com>
2025-05-23 14:33:45 +0800

    Added function to find point of entry via last closed flux surface. Updated pol vec calculations to be p=kxb at point of entry. Formatting and spelling. Added option to save.

--- scotty2ERMES.py
+++ scotty2ERMES.py
@@ -5,7 +5,8 @@ Transposes ne data to R,Z coords as well
 Spits out .txt and .csv files of all the necessary data. 
 
 TODO
-1. Run in ERMES and compare to scotty
+1. Run in ERMES and compare to scotty (7 deg done, 15 deg done)
+2. Linear to Elliptical pol
 
 References
     [1] Two dimensional full-wave simulations of Doppler back-scattering in tokamak plasmas with COMSOL by Quinn Pratt et al (in-progress paper)
@@ -14,25 +15,66 @@ References
 
 Written by Dylan James Mc Kaige
 Created: 16/5/2025
-Updated: 20/5/2025
+Updated: 23/5/2025
 """
 import os, json
 import numpy as np
 import pandas as pd
-from math import sin, cos, sqrt, fabs
+from math import sin, cos, sqrt, fabs, isclose
 from scipy.constants import c, pi
-from scipy.interpolate import RectBivariateSpline, UnivariateSpline
+from scipy.interpolate import RectBivariateSpline, UnivariateSpline, RegularGridInterpolator
 from matplotlib import pyplot as plt
 
-def ne_pol_to_RZ(
+def find_lcfs_entry_point(pol_flux_spline, R0, Z0, launch_angle_rad, psi_closed, step=0.001, max_dist=0.5):
+    """
+    Finds the point of entry along the Last Closed Flux Surface for calculation of polarization vector (Linear)
+
+    Args:
+        pol_flux_spline (spline): Poloidal flux spline
+        R0 (float): R of launcher (or centre of port since they are collinear)
+        Z0 (float): Z of launcher (or centre of port since they are collinear)
+        launch_angle_rad (float): Launch angle in rad w.r.t -ve R axis
+        psi_closed (float): Value of poloidal flux for the LCFS
+        step (float): Step size for line interpolation. Defaults to 0.001
+        max_dist (floattional): Maximum distance away from launcher (or port) to search. Defaults to 0.5
+
+    Returns:
+        R_entry (float): R of point of entry
+        Z_entry (float): Z of point of entry
+    """
+    s_vals = np.arange(0, max_dist, step)
+    R_vals = R0 - s_vals * cos(-launch_angle_rad)
+    Z_vals = Z0 - s_vals * sin(-launch_angle_rad)
+
+    psi_vals = pol_flux_spline.ev(R_vals, Z_vals)
+    for i in range(len(psi_vals) - 1):
+        psi1 = psi_vals[i]
+        psi2 = psi_vals[i + 1]
+
+        if (psi1 < psi_closed and psi2 >= psi_closed) or (psi1 > psi_closed and psi2 <= psi_closed):
+            
+            # Linear interpolation between psi1 and psi2
+            frac = (psi_closed - psi1)/(psi2 - psi1 + 1e-12)
+            s_entry = s_vals[i] + frac * step
+            R_entry = R0 - s_entry*cos(-launch_angle_rad)
+            Z_entry = Z0 - s_entry*sin(-launch_angle_rad)
+            return R_entry, Z_entry
+
+    raise "Last Closed Flux Surface not intersected along beam path"
+    
+
+def scottyData2ERMES(
     ne_path: str,
     topfile_path: str,
+    filename: str,
     plot = True,
     save = True,
     path = os.getcwd() + '\\',
     ERMES_R = None,
     ERMES_Z = None,
     ERMES_port = None,
+    ERMES_launch_centre = None,
+    launch_angle_rad = None,
     num_RZ = 20
     ):
     """
@@ -47,8 +89,13 @@ def ne_pol_to_RZ(
         ERMES_R (tuple): Range of R to zoom in and save ne of defaults to full range from topfile
         ERMES_Z (tuple): Range of Z to zoom in and save ne of defaults to full range from topfile
         ERMES_port (tuple): Coordinates of port to check if it is within the plasma 
+        ERMES_launch_centre (tuple): Coordinates of centre of launch point in ERMES
+        launch_angle_rad (float): Launch angle in rad w.r.t -ve R axis
+        num_RZ (int): Number of RZ points for ERMES
         
     Returns:
+        entry_point (array): Coordinates of point of entry defined by intersection of the last closed flux surface and the initial launch beam
+        B_entry_vec (array): B field at entry point of beam into plasma
         Plots ne in R,Z space w.r.t pol flux and saves a .csv files of ne, R, Z, Br, Bt, Bz for ERMES (Need to be converted using fullwavedensfile.py & fullwavemagfile.py)
     """
     path = os.getcwd() + '\\'
@@ -93,17 +140,33 @@ def ne_pol_to_RZ(
     Br_vals_ERMES = Br_spline.ev(R_grid_to_ERMES, Z_grid_to_ERMES)
     Bz_vals_ERMES = Bz_spline.ev(R_grid_to_ERMES, Z_grid_to_ERMES)
     Bt_vals_ERMES = Bt_spline.ev(R_grid_to_ERMES, Z_grid_to_ERMES)
+    
+    # Get the line from launcher to plasma then find the point where ne goes from 0 to +ve, this is the point of entry of the beam into the plasma
+    entry_point = find_lcfs_entry_point(
+        pol_flux_spline,
+        R0=ERMES_launch_centre[0],
+        Z0=ERMES_launch_centre[1],
+        launch_angle_rad=launch_angle_rad,
+        psi_closed=ne_data[0,-1],
+        
+    )
+    B_entry_vec = np.array([
+        Br_spline.ev(entry_point[0], entry_point[1]),
+        Bz_spline.ev(entry_point[0], entry_point[1]),
+        Bt_spline.ev(entry_point[0], entry_point[1])
+        ]
+    )
  
     RZ_ERMES = np.column_stack((R_range, Z_range))
 
     if save:
-        np.savetxt("RZ_ERMES.csv", RZ_ERMES, delimiter=",", fmt="%.6e")
+        np.savetxt(filename + "RZ_ERMES.csv", RZ_ERMES, delimiter=",", fmt="%.6e")
 
-        np.savetxt("Br_ERMES.csv", Br_vals_ERMES.T, delimiter=",", fmt="%.6e")
-        np.savetxt("Bz_ERMES.csv", Bz_vals_ERMES.T, delimiter=",", fmt="%.6e")
-        np.savetxt("Bt_ERMES.csv", Bt_vals_ERMES.T, delimiter=",", fmt="%.6e")
+        np.savetxt(filename + "Br_ERMES.csv", Br_vals_ERMES.T, delimiter=",", fmt="%.6e")
+        np.savetxt(filename + "Bz_ERMES.csv", Bz_vals_ERMES.T, delimiter=",", fmt="%.6e")
+        np.savetxt(filename + "Bt_ERMES.csv", Bt_vals_ERMES.T, delimiter=",", fmt="%.6e")
 
-        np.savetxt("ne_ERMES.csv", ne_vals_to_ERMES.T, delimiter=",", fmt="%.6e")
+        np.savetxt(filename + "ne_ERMES.csv", ne_vals_to_ERMES.T, delimiter=",", fmt="%.6e")
     
     if plot:
         plt.figure(figsize=(8, 6))
@@ -124,8 +187,9 @@ def ne_pol_to_RZ(
         cf = plt.contourf(R_grid_to_ERMES, Z_grid_to_ERMES, ne_vals_to_ERMES, levels=100, cmap='plasma')
         pol_flux_levels = np.linspace(0.0, 1.0, 11)
         cs = plt.contour(R_grid_to_ERMES, Z_grid_to_ERMES, pol_flux_vals_to_ERMES, levels=pol_flux_levels, colors='black', linewidths=0.8)
-        plt.scatter(*zip(*ERMES_port), s = 2, color='white')
         plt.clabel(cs, fmt=r'%.1f', fontsize=8, colors='black')
+        plt.scatter(*zip(*ERMES_port), s = 2, color='white')
+        plt.scatter(*zip(entry_point), s = 2, color='white')
 
         plt.colorbar(cf, label=r'$n_e\ (10^{19}\ \mathrm{m}^{-3})$')
         plt.xlabel("R [m]")
@@ -134,6 +198,8 @@ def ne_pol_to_RZ(
         plt.axis('equal')
         plt.tight_layout()
         plt.show()
+        
+    return entry_point, B_entry_vec
 
 def get_ERMES_parameters(
     launch_angle: float,
@@ -147,7 +213,6 @@ def get_ERMES_parameters(
     plot = True,
     save = True,
     path = os.getcwd() + '\\',
-    gen_ne = True,
     ne_path = None,
     topfile_path = None,
     num_RZ = 20,
@@ -167,7 +232,6 @@ def get_ERMES_parameters(
         plot (bool): Plot everything
         save (bool): Save everything
         path (str): Path to save file in, defaults to cwd
-        gen_ne (bool): Generte ne, R and Z files for ERMES
         ne_path (str): Relative (to cwd) paht of ne.dat file
         topfile_path (str): Relative (to cwd) path of topfile.json file
         num_RZ (int): Number of RZ points for ERMES
@@ -176,6 +240,7 @@ def get_ERMES_parameters(
         Plots the position of the required points in the Z-R axes and saves a .txt file of the necessary values for ERMES
         Optionally generates the necessary ne, R and Z files for ERMES
     """
+    filename = str(launch_angle) + "_degree_" + str(launch_freq_GHz) + "GHz_"
     degtorad = pi/180
     
     launch_beam_wavelength = c/(launch_freq_GHz*1e9)
@@ -195,14 +260,6 @@ def get_ERMES_parameters(
     xw = launch_R - distance_to_launcher*cos(launch_angle_rad) # Centre of waist
     yw = launch_Z + distance_to_launcher*sin(launch_angle_rad) # Centre of waist
     
-    # Scuffed normalized polarization vector generation (Forcing it to be polarized out of the plane of the screen)
-    # Since we define k to be in the plane of the screen, the vector pointing out is definitely perp to it, satisfying ERMES
-    # TODO update this to get the actual one from scotty
-    # THIS JUST ENSURES THAT ERMES WORKS, it is very wrong please update this 
-    polx = 0
-    poly = 0
-    polz = 1
-    
     # This formula is from a .txt file from UCLA collaborators. I didn't look for a proper source/derivation of this yet
     z0 = 377 # Impedance of free space
     E0 = sqrt(z0*2*1/(w0*sqrt(pi/2))) # For P_in = 1 W/m
@@ -216,82 +273,123 @@ def get_ERMES_parameters(
     
     # Domain calculations
     # Arbitrary padding (~10 x order of mesh spacing?) so that the volume can be generated. 
-    # If padding is too small, will cause errors in ERMES
+    # If padding is too small, will cause errors in ERMES OR mesh generation will take stupidly long... I don't make the rules!
     xd1, yd0 = xp11 + 0.005, yp01 - 0.005 
     xd0, yd1 = xd1-domain_size, yd0+domain_size
 
+    # Scotty data calculations
+    entry_point, B_entry_vec = scottyData2ERMES(
+        ne_path, 
+        topfile_path, 
+        filename = filename,
+        plot=plot,
+        ERMES_R=(xd0, xd1), 
+        ERMES_Z=(yd0, yd1), 
+        ERMES_port=([xp0, yp0], [xp1, yp1], [xp01, yp01], [xp11, yp11]), 
+        ERMES_launch_centre=(xp, yp), 
+        launch_angle_rad=launch_angle_rad,
+        num_RZ=num_RZ,
+        )
+    
+    # pol vec at launch point = beam k X B field at launch point
+    k_vec = np.array([kx_norm, ky_norm, 0])
+    pol_vec = np.cross(k_vec, B_entry_vec)
+    pol_vec = pol_vec/np.linalg.norm(pol_vec)
+    
     # Arrays for saving
     # Surely there is a neater way of doing this, but I'm lazy and want to get this working first before making it pretty
-    points_x = np.array(
-        [xp, xp0, xp1, xp01, xp11, launch_R, xd0, xd1, xd0, xd1, xw]
+    points_x = np.array([
+        xp, 
+        xp0, 
+        xp1, 
+        xp01, 
+        xp11, 
+        launch_R, 
+        xd0, 
+        xd1, 
+        xd0, 
+        xd1, 
+        xw,
+        entry_point[0],
+        ]
     )
-    points_y = np.array(
-        [yp, yp0, yp1, yp01, yp11, launch_Z, yd0, yd0, yd1, yd1, yw]
+    points_y = np.array([
+        yp, 
+        yp0, 
+        yp1, 
+        yp01, 
+        yp11, 
+        launch_Z, 
+        yd0, 
+        yd0, 
+        yd1, 
+        yd1, 
+        yw,
+        entry_point[1],
+        ]
     )
-    points_names = np.array(
-        ['Source Position (front face of port)    ', 
-         'Port BL    ', 
-         'Port TL    ', 
-         'Port BR    ', 'Port TR    ', 
-         'Launch Position    ', 
-         'Domain BL    ', 
-         'Domain BR    ', 
-         'Domain TL    ', 
-         'Domain TR    ', 
-         'Waist Position    '
-         ]
+    points_names = np.array([
+        'Source Position (front face of port)    ', 
+        'Port BL    ', 
+        'Port TL    ', 
+        'Port BR    ', 'Port TR    ', 
+        'Launch Position    ', 
+        'Domain BL    ', 
+        'Domain BR    ', 
+        'Domain TL    ', 
+        'Domain TR    ', 
+        'Waist Position',
+        'Point of Entry',
+        ]
     )
 
     # Beam params
-    params_val = np.array(
-        [launch_angle, 
-         launch_beam_width, 
-         radius_of_curv, 
-         distance_to_launcher, 
-         dist_to_ERMES_port, 
-         w0, 
-         z_R, 
-         port_width, 
-         launch_freq_GHz, 
-         launch_beam_wavelength, 
-         kx_norm, 
-         ky_norm, 
-         polx, 
-         poly, 
-         polz, 
-         E0, 
-         ]
+    params_val = np.array([
+        launch_angle, 
+        launch_beam_width, 
+        radius_of_curv, 
+        distance_to_launcher, 
+        dist_to_ERMES_port, 
+        w0, 
+        z_R, 
+        port_width, 
+        launch_freq_GHz, 
+        launch_beam_wavelength, 
+        kx_norm, 
+        ky_norm, 
+        pol_vec[0],
+        pol_vec[1],
+        pol_vec[2],
+        E0, 
+        ]
     )
-    params_names = np.array(
-        ['Launch Angle    ', 
-         'Launch Beam Width    ', 
-         'launch Beam Radius of Curvature    ', 
-         'Distance to Launcher (from waist)    ', 
-         'Distance to ERMES Port (from launcher)    ', 
-         'Beam Waist (w0)    ', 
-         'Rayleigh Length (m)    ', 
-         'Port Width    ', 
-         'Beam Frequency (GHz)    ', 
-         'Beam Wavelength (m)    ', 
-         'kx (normalized)    ', 
-         'ky (normalized)    ', 
-         'polx (normalized)    ', 
-         'poly (normalized)    ', 
-         'polz (normalized)    ', 
-         'E0    ', 
-         ]
+    params_names = np.array([
+        'Launch Angle    ', 
+        'Launch Beam Width    ', 
+        'launch Beam Radius of Curvature    ', 
+        'Distance to Launcher (from waist)    ', 
+        'Distance to ERMES Port (from launcher)    ', 
+        'Beam Waist (w0)    ', 
+        'Rayleigh Length (m)    ', 
+        'Port Width    ', 
+        'Beam Frequency (GHz)    ', 
+        'Beam Wavelength (m)    ', 
+        'kx (normalized)    ', 
+        'ky (normalized)    ', 
+        'polx (normalized)    ', 
+        'poly (normalized)    ', 
+        'polz (normalized)    ', 
+        'E0    ', 
+        ]
     )
     
     # Save it!
     if save:
-        np.savetxt(path + 'ERMES_params', np.array([points_names, points_x, points_y], dtype=object).T, delimiter=' ', header = 'Cartesian points in ERMES', fmt='%s')
+        np.savetxt(path + filename + 'ERMES_params', np.array([points_names, points_x, points_y], dtype=object).T, delimiter=' ', header = 'Cartesian points in ERMES', fmt='%s')
         # There's probably a better way to do this 
-        with open(path + 'ERMES_params', 'a') as file:
+        with open(path + filename + 'ERMES_params', 'a') as file:
             np.savetxt(file, np.array([params_names, params_val], dtype=object).T, delimiter=' ', header='Beam Params', fmt = '%s')
-    
-    if gen_ne:
-        ne_pol_to_RZ(ne_path, topfile_path, ERMES_R = (xd0, xd1), ERMES_Z = (yd0, yd1), ERMES_port = ([xp0, yp0], [xp1, yp1], [xp01, yp01], [xp11, yp11]), num_RZ = num_RZ, plot=plot)
-    
+
     if plot:
         plt.scatter(points_x, points_y, s = 2)
         plt.gca().set_aspect('equal')
@@ -299,17 +397,17 @@ def get_ERMES_parameters(
 
 if __name__ == '__main__':
     get_ERMES_parameters(
-        launch_angle=7.0, 
+        launch_angle=15.0, 
         launch_freq_GHz=72.5, 
         port_width=0.01, 
         launch_positon=[3.01346,0,-0.09017], 
         launch_beam_curvature=-0.95, 
         launch_beam_width=0.125, 
         dist_to_ERMES_port=0.65, 
-        domain_size=0.3, 
+        domain_size=0.45, 
         ne_path = '\\source_data\\ne_189998_3000ms_quinn.dat', 
         topfile_path = '\\source_data\\topfile_189998_3000ms_quinn.json',
         num_RZ = 25,
         plot=True,
-        save = False,
+        save=False,
         )
