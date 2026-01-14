dcc776d6b5bdd91ad1bbc340fb68c069f2a2987d
mckaigedj <dylan.j.mckaige@gmail.com>
2026-01-14 11:14:19 +0000

    Comments for clarity

--- createVectorX.py
+++ createVectorX.py
@@ -1,9 +1,9 @@
 """
-Jank way of getting ERMES and PETSc to work. Taken from various PETSc and ERMES scripts.
+Jank way of getting ERMES and PETSc to work if you can't have it automatically run. 
+
+Taken from various PETSc and ERMES scripts.
 Creates the X vector file for ERMES to read.
 
-TODO
-1. Take user input in terminal for path
 
 Written by Dylan James Mc Kaige
 Created 14/5/2025
@@ -13,12 +13,14 @@ Updated 19/5/2025
 import numpy as np
 
 # GiD problem folder path, MANUALLY CHANGE THIS
-FolderPath = '/home/chenvh/mckaigedj/test.gid/'
+FolderPath = '/to/tour/.gid'
+print( '------------------------------------------------------------' )
+print( 'Creating X matrix' )
 X_vector = np.array( np.fromfile(FolderPath + 'Vector_X_PETSc.dat', dtype=np.dtype( ( np.float64, 2 ) ) ) )
 X_vector = np.array( X_vector ).byteswap()
 NewFile = open( FolderPath + 'Vector_Xo.bin', 'wb' )
 X_vector.tofile(NewFile)
 NewFile.close()
-
-print( '---------------------------------------------------------' )
 print('Done, calculate ERMES in READ mode')
+print( '---------------------------------------------------------' )
+
